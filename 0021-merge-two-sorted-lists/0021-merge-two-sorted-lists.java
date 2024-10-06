81ddb68fc7bcb4b4402e43ab8f473586cab564df
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2024-10-05 22:58:34 -0700

    Time: 0 ms (100%), Space: 42.8 MB (9.47%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -11,38 +11,36 @@
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
 
-        // Handle cases where either list is empty
-        if (list1 == null) return list2;
-        if (list2 == null) return list1;
-
-        // Determine the head of the merged list
-        ListNode head;
-        if (list1.val <= list2.val) {
-            head = list1;
+ ListNode dummy = new ListNode(0);
+        ListNode current = dummy;
+        while(list1 != null && list2 != null){
+           if(list1.val < list2.val){
+            current.next = list1;
             list1 = list1.next;
-        } else {
-            head = list2;
+           }
+           else{
+            current.next = list2;
             list2 = list2.next;
+           }
+           current = current.next; 
         }
+        current.next= list1 != null ? list1 :list2; 
+        return dummy.next;
 
-        // Set the current node to the head of the merged list
-        ListNode current = head;
+    }
 
-        // Iterate through both lists and merge
-        while (list1 != null && list2 != null) {
-            if (list1.val <= list2.val) {
-                current.next = list1;
-                list1 = list1.next;
-            } else {
-                current.next = list2;
-                list2 = list2.next;
-            }
-            current = current.next;
-        }
+    public static void main(String args[]){
+        ListNode list1 = new ListNode(1);
+        list1.next = new ListNode(2);
+        list1.next.next = new ListNode(2);
 
-        // Attach the remaining part of the non-exhausted list
-        current.next = (list1 != null) ? list1 : list2;
+        ListNode list2 = new ListNode(1);
+        list2.next = new ListNode(3);
+        list2.next.next = new ListNode(4);
 
-        return head;
+        // Test mergeTwoLists
+        Solution solution = new Solution();
+        ListNode mergedList = solution.mergeTwoLists(list1, list2);
+
     }
 }
