7089e410ef1e115d42da1c77c3e4eed2da20efc2
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2025-05-01 15:08:45 -0700

    Time: 0 ms (100%), Space: 44.9 MB (40.47%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.java
@@ -1,42 +1,24 @@
 class Solution {
     public int removeDuplicates(int[] nums) {
-        int[] temp = new int[nums.length];
-        int i = 0;
-        int total = 0;
-        for(int k = 0; k < nums.length; k++){
-            if(k == nums.length - 1){
-                if(temp[i] != nums[nums.length - 1]){
-                    temp[i] = nums[nums.length - 1];
-                    total++;
+        int count = 0;
+        int temp = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            if(i == 0) {
+                temp = nums[i];
+                count++;
+            } else {
+                if(temp != nums[i]) {
+                    temp = nums[i];
+                    nums[count] = temp;
+                    count++;
                 }
             }
-            else if (nums[k] == nums[k+1]){
-                int count = 1;
-                total ++;
-                int j = k+2;
-                while( j < nums.length){
-                    if(nums[k] == nums[j]){
-                        count++;
-                    }
-                    j++;
-                }
-                temp[i] = nums[k];
-                i++;
-                k += count;
-            }else{
-                temp[i] = nums[k];
-                total++;
-                i++;
-            }
-        }
-        // for(int num: temp){print(num+"");}
-        for(int z = 0; z < nums.length; z++){
-            if(z < total){nums[z] = temp[z];}
-            else{nums[z] = 0;}
         }
 
-        return temp.length - (temp.length-total);
+        //System.out.println(Arrays.toString(nums));
+        //System.out.println(count);
+
+        return count;
     }
-
-    public static void print(String value){ System.out.print(value);}
 }
