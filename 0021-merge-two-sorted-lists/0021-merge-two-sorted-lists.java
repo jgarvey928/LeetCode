87f1fbac7041ce2fc45582834c6de8f6131ca7e4
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2026-03-12 17:09:59 -0700

    Time: 0 ms (100%), Space: 44.1 MB (86.01%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -1,46 +1,39 @@
 /**
  * Definition for singly-linked list.
  * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
 class Solution {
     public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
-
- ListNode dummy = new ListNode(0);
+        // Create a dummy node to act as the starting point of the merged list
+        ListNode dummy = new ListNode(-1);
         ListNode current = dummy;
-        while(list1 != null && list2 != null){
-           if(list1.val < list2.val){
-            current.next = list1;
-            list1 = list1.next;
-           }
-           else{
-            current.next = list2;
-            list2 = list2.next;
-           }
-           current = current.next; 
+
+        // Iterate while both lists have nodes
+        while (list1 != null && list2 != null) {
+            if (list1.val <= list2.val) {
+                current.next = list1;
+                list1 = list1.next;
+            } else {
+                current.next = list2;
+                list2 = list2.next;
+            }
+            current = current.next;
         }
-        current.next= list1 != null ? list1 :list2; 
+
+        // If one list is exhausted, attach the remainder of the other list
+        if (list1 != null) {
+            current.next = list1;
+        } else {
+            current.next = list2;
+        }
+
+        // Return the head of the merged list (skip the dummy node)
         return dummy.next;
-
-    }
-
-    public static void main(String args[]){
-        ListNode list1 = new ListNode(1);
-        list1.next = new ListNode(2);
-        list1.next.next = new ListNode(2);
-
-        ListNode list2 = new ListNode(1);
-        list2.next = new ListNode(3);
-        list2.next.next = new ListNode(4);
-
-        // Test mergeTwoLists
-        Solution solution = new Solution();
-        ListNode mergedList = solution.mergeTwoLists(list1, list2);
-
     }
 }
