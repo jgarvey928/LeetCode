f869d612035b2c9027ff1f4abc6b35e23072052b
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2026-03-12 17:18:58 -0700

    Time: 1 ms (99.82%), Space: 94.4 MB (83.27%) - LeetHub

--- 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
+++ 0121-best-time-to-buy-and-sell-stock/0121-best-time-to-buy-and-sell-stock.java
@@ -1,21 +1,21 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int min = prices[0];
-        int size = prices.length;
-        int max_diff = 0;
-        for(int i = 0; i < size; i++){
-            for(int j = i+1; j < size; j++){
-                if(prices[j] > min){
-                    if(prices[j] - min > max_diff){
-                        max_diff = prices[j] - min;
-                    }
-                }else{
-                    min = prices[j];
-                    i = j - 1;
-                    break;
-                }
+        // Initialize the minimum price to a very high value
+        int minPrice = Integer.MAX_VALUE;
+        // Initialize the maximum profit to 0
+        int maxProfit = 0;
+        
+        for (int i = 0; i < prices.length; i++) {
+            // Update minPrice if the current price is lower
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } 
+            // Calculate profit if sold today and update maxProfit if it's higher
+            else if (prices[i] - minPrice > maxProfit) {
+                maxProfit = prices[i] - minPrice;
             }
         }
-        return max_diff;
+        
+        return maxProfit;
     }
 }
