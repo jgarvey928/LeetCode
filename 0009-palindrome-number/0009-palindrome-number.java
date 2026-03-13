3949a7ca7d820ceb3f90779bf072936be9f59f11
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2026-03-12 17:09:05 -0700

    Time: 5 ms (79.49%), Space: 45.4 MB (99%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.java
+++ 0009-palindrome-number/0009-palindrome-number.java
@@ -1,15 +1,24 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        String s = String.valueOf(x); // Convert to String
-        int n = s.length(); // Store the String length to int n
-
-        for (int i=0; i<n/2; i++) {
-            // We check whether the elements at the same distance from
-            // beginning and from ending are same, if not we return false
-            if (s.charAt(i) != s.charAt(n-i-1)) return false;
+        // Special cases:
+        // As discussed in the problem constraints, negative numbers are not palindromes.
+        // Also, if the last digit of the number is 0, for the number to be a palindrome,
+        // the first digit of the number also needs to be 0.
+        // Only 0 satisfy this property.
+        if (x < 0 || (x % 10 == 0 && x != 0)) {
+            return false;
         }
 
-        // if no flaws are found we return true
-        return true;
+        int revertedNumber = 0;
+        while (x > revertedNumber) {
+            revertedNumber = revertedNumber * 10 + x % 10;
+            x /= 10;
+        }
+
+        // When the length is an odd number, we can get rid of the middle digit by revertedNumber/10
+        // For example when the input is 12321, at the end of the while loop we get x = 12, revertedNumber = 123,
+        // since the middle digit doesn't matter in palindrome(it will always equal to itself), 
+        // we can simply get rid of it.
+        return x == revertedNumber || x == revertedNumber / 10;
     }
 }
