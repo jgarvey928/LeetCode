523aabf2044e829547412c4562541f9f7c66cb6e
John S. Garvey <65931998+jgarvey928@users.noreply.github.com>
2026-03-12 17:23:07 -0700

    Time: 0 ms (100%), Space: 110.7 MB (91.96%) - LeetHub

--- 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
+++ 1791-find-center-of-star-graph/1791-find-center-of-star-graph.java
@@ -1,12 +1,17 @@
 class Solution {
     public int findCenter(int[][] edges) {
-        int value1 = edges[0][0];
-        if(value1 == edges[1][0]){
-            return value1;
-        }
-        if(value1 == edges[1][1]){
-            return value1;
-        }
-        return edges[0][1];
+        // In a star graph, the center node must appear in every edge.
+        // We only need to compare the two nodes of the first edge 
+        // with the nodes of the second edge.
+        
+        int u1 = edges[0][0];
+        int v1 = edges[0][1];
+        
+        int u2 = edges[1][0];
+        int v2 = edges[1][1];
+        
+        // If u1 is present in the second edge, it is the center.
+        // Otherwise, v1 must be the center.
+        return (u1 == u2 || u1 == v2) ? u1 : v1;
     }
 }
